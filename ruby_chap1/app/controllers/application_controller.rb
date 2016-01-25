5c4547b4e6198a3f441d9fe8a3cc881a14780db1
ThuanDV <dauthan95@gmail.com>
2016-01-25 22:22:58 +0700

    lam het bai tap chap 1

--- ruby_chap1/app/controllers/application_controller.rb
+++ ruby_chap1/app/controllers/application_controller.rb
@@ -3,6 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   def hello
-    render text: "hello, world!"
+  # render text: "hello, world!"
+    render text: "hello, my name is thuan! ¡Hola, mundo"
+  end
+
+  def goodbye
+  # render text: "hello, world!"
+    render text: "goodbye world!"
   end
 end
