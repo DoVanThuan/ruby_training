caf507b74033cf33fe7dc6ddd775c8d4d10b6679
ThuanDV <dauthan95@gmail.com>
2016-01-29 23:55:18 +0700

    bai tap chap 4

---
+++ chap4/chap4_ex.rb
@@ -0,0 +1,9 @@
+#bt 3
+person1 = {":first" => "firstname1", ":last" => "lastname1"}
+person2 = {":first" => "firstname2", ":last" => "lastname2"}
+person3 = {":first" => "firstname3", ":last" => "lastname3"}
+params = {":father"=> person1, ":mother" => person2, ":child" => person3}
+
+#bt 4
+
+
